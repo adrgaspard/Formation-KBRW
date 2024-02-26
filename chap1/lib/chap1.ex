8f2f3282351c824d64f8fdd81ed29e1435898ad8
Gaspard <gaspard.andrieu@kbrw.com>
2024-02-26 09:49:37 +0100

    Correction du chemin de fichier de ressource dans le chapitre 1

--- chap1/lib/chap1.ex
+++ chap1/lib/chap1.ex
@@ -7,9 +7,8 @@ defmodule Chap1 do
     ]
     pid = Supervisor.start_link(children, strategy: :one_for_one)
     0..1
-    |> Enum.map(fn num -> "../../_instructions/Resources/chap1/orders_dump/orders_chunk#{num}.json" end)
+    |> Enum.map(fn num -> "../_instructions/Resources/chap1/orders_dump/orders_chunk#{num}.json" end)
     |> Enum.each(fn file -> JsonLoader.load_to_database :db_default, file end)
     pid
   end
-
 end
