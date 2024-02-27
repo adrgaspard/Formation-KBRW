1f26578df4a5e756c3800522d49574d92b1dbce9
Gaspard <gaspard.andrieu@kbrw.com>
2024-02-27 15:12:02 +0100

    Chapitre 4 étape 3

--- chap4/web/app.js
+++ chap4/web/app.js
@@ -9,11 +9,59 @@ const createReactClass = require("create-react-class");
 require("./webflow/css/order.css");
 require("./webflow/css/orders.css");
 
+const Orders = [
+  {
+    remoteid: "000000189",
+    custom: {
+      customer: { full_name: "TOTO & CIE" },
+      billing_address: "Some where in the world",
+    },
+    items: 2,
+  },
+  {
+    remoteid: "000000190",
+    custom: {
+      customer: { full_name: "Looney Toons" },
+      billing_address: "The Warner Bros Company",
+    },
+    items: 3,
+  },
+  {
+    remoteid: "000000191",
+    custom: {
+      customer: { full_name: "Asterix & Obelix" },
+      billing_address: "Armorique",
+    },
+    items: 29,
+  },
+  {
+    remoteid: "000000192",
+    custom: {
+      customer: { full_name: "Lucky Luke" },
+      billing_address: "A Cowboy doesn't have an address. Sorry",
+    },
+    items: 0,
+  },
+];
+
 const Page = createReactClass({
   render() {
-    return <JSXZ in="orders" sel=".container"></JSXZ>;
+    return Orders.map(order => (
+      <JSXZ in="orders" sel=".orderlinebody">
+        <Z sel=".command-number > .text-block">{order.remoteid}</Z>
+        <Z sel=".customer > .text-block">{order.custom.customer.full_name}</Z>
+        <Z sel=".address > .text-block">{order.custom.billing_address}</Z>
+        <Z sel=".quantity > .text-block">{order.items}</Z>
+      </JSXZ>
+    ));
   },
 });
+
+// const Page = createReactClass({
+//   render() {
+//     return <JSXZ in="orders" sel=".container"></JSXZ>;
+//   }
+// })
 
 const domNode = document.getElementById("root");
 console.log(domNode);
