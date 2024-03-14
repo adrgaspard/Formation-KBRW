c0b896e5dd6468353cfaf967d6f2c6107bd67350
Gaspard <gaspard.andrieu@kbrw.com>
2024-03-14 16:37:01 +0100

    Chapitre 8 étape 1

---
+++ chap8/lib/order_fsm.ex
@@ -0,0 +1,17 @@
+defimpl ExFSM.Machine.State, for: Map do
+  def state_name(order), do: String.to_atom(order["status"]["state"])
+  def set_state_name(order, name), do: put_in(order, ["status", "state"], Atom.to_string(name))
+  def handlers(order) do: [OrderFSM]
+end
+
+defmodule OrderFSM do
+  use ExFSM
+
+  deftrans init({:process_payment, []}, order) do
+    {:next_state, :not_verified, order}
+  end
+
+  deftrans not_verified({:verification, []}, order) do
+    {:next_state, :finished, order}
+  end
+end
